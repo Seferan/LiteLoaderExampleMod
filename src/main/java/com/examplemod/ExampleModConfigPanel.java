93696d02a1926eaaa5a3d283978f3e60298182d7
Mumfrey <adam@eq2.co.uk>
2017-08-07 15:09:34 +0100

    Add examples for config panel text fields

--- src/main/java/com/examplemod/ExampleModConfigPanel.java
+++ src/main/java/com/examplemod/ExampleModConfigPanel.java
@@ -4,6 +4,7 @@ import com.mumfrey.liteloader.Configurable;
 import com.mumfrey.liteloader.client.gui.GuiCheckbox;
 import com.mumfrey.liteloader.modconfig.AbstractConfigPanel;
 import com.mumfrey.liteloader.modconfig.ConfigPanelHost;
+import com.mumfrey.liteloader.util.log.LiteLoaderLogger;
 import net.minecraft.client.resources.I18n;
 
 /**
@@ -15,6 +16,9 @@ import net.minecraft.client.resources.I18n;
  */
 public class ExampleModConfigPanel extends AbstractConfigPanel
 {
+    // Preserve references to the text field handles so we can read the values
+    private ConfigTextField txtString, txtNumeric;
+    
     /* (non-Javadoc)
      * @see com.mumfrey.liteloader.modconfig.ConfigPanel#getPanelTitle()
      */
@@ -42,6 +46,9 @@ public class ExampleModConfigPanel extends AbstractConfigPanel
                 mod.setClockVisibility(control.checked = !control.checked);
             }
         }).checked = mod.getClockVisibility();
+        
+        this.txtString = this.addTextField(2, 0, 50, 200, 20).setMaxLength(255);
+        this.txtNumeric = this.addTextField(3, 0, 75, 200, 20).setRegex("^[0-9]*$", false).setMaxLength(6);
     }
 
     @Override
@@ -50,5 +57,24 @@ public class ExampleModConfigPanel extends AbstractConfigPanel
         // This example applies the changes immediately, however you may wish to
         // only save changes when the user clicks "save and close". In which
         // case you should apply your changes here
+        
+        
+        // Code below shows how to read and handle the values out of the text
+        // fields, what you do with the values is up to you.
+        
+        LiteLoaderLogger.info("Text field value was %s", this.txtString.getText());
+        
+        if (this.txtNumeric.isValid())
+        {
+            // Prefix 0 on the value since an empty string is valid, this saves
+            // us having to special-case the string being empty. Alternatively
+            // we could disallow empty strings in the validation regex.
+            int intValue = Integer.parseInt("0" + this.txtNumeric.getText());
+            LiteLoaderLogger.info("Numeric field value was %d", intValue);
+        }
+        else
+        {
+            LiteLoaderLogger.info("Numeric field value was not valid!");
+        }
     }
 }
